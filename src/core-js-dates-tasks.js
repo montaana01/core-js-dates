795db169da0a04b5a88cbe14ceacb57d0b9a07de
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-03-15 22:13:22 +0300

    feat: time formatter complete

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -161,8 +161,20 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dateToFormat = new Date(date);
+
+  const month = dateToFormat.getUTCMonth() + 1;
+  const day = dateToFormat.getUTCDate();
+  const year = dateToFormat.getUTCFullYear();
+
+  let hours = dateToFormat.getUTCHours();
+  const minutes = dateToFormat.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = dateToFormat.getUTCSeconds().toString().padStart(2, '0');
+  const halfOfDay = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12 || 12;
+
+  return `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${halfOfDay}`;
 }
 
 /**
